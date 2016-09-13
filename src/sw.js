eec1e92b8f85a4e5b7640184427101a19072a94d
theKMF <tigerabbit@gmail.com>
2016-09-13 10:39:30 +0200

    One more go
    Forgot to save the newly created file before commit

--- src/sw.js
+++ src/sw.js
@@ -1,23 +1,3 @@
-var audio = new Audio('src/sfx.mp3');
-
-
-DomReady.ready(function () {
-    if(navigator.serviceWorker.register('src/sw.js');
-
-    shaverBtn.addEventListener('touchstart', function(e){
-       beginShave();
-	}, false);
-	
-    shaverBtn.addEventListener('touchend', boxTouchEnd, false);
-
+self.addEventListener('fetch', event => { 
+    debugger;
 });
-
-function beginShave(e){
-    audio.play();
-    window.navigator.vibrate(9300);
-}
-function boxTouchEnd(e){
-    audio.pause();
-    window.navigator.vibrate(0);
-    audio.currentTime = 0;
-}
