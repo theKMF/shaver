9f46adee50b3937393f34296636818502deb6727
theKMF <tigerabbit@gmail.com>
2016-09-14 08:00:20 +0200

    Big changus
    moved the sw register into DomREady natch lol

--- src/script.js
+++ src/script.js
@@ -1,10 +1,16 @@
-var audio = new Audio('src/sfx.mp3');
-if(navigator.serviceWorker){
-    navigator.serviceWorker.register('/sw.js');
-}
+var audio;
 
 
 DomReady.ready(function () {
+    
+    if ('serviceWorker' in navigator) {
+        console.log('navigator available');
+        navigator.serviceWorker.register('/sw.js').then(function(reg) {
+        console.log('ServiceWorker registration successful with scope: ', reg.scope);
+    }).catch(function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+    });
+}
    
     shaverBtn.addEventListener('touchstart', function(e){
        beginShave();
@@ -13,6 +19,10 @@ DomReady.ready(function () {
     shaverBtn.addEventListener('touchend', boxTouchEnd, false);
 
 });
+
+function setUp(){
+    audio = new Audio('src/sfx.mp3');
+}
 
 function beginShave(e){
     audio.play();
